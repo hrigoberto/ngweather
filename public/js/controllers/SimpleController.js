dd64937a1acc1c20e54ce7642f3c22200e0cefa0
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-07-21 20:52:08 -0400

    finished weather service

--- public/js/controllers/SimpleController.js
+++ public/js/controllers/SimpleController.js
@@ -7,6 +7,8 @@
   function SimpleController($scope, WeatherService){
     $scope.message = 'Yooo';
     WeatherService.getHourlyData(29, -82);
+    WeatherService.getMinutelyData(29, -82);
+    WeatherService.getDailyData(29, -82);
   };
 
 })();
--- public/js/services/WeatherService.js
+++ public/js/services/WeatherService.js
@@ -7,6 +7,11 @@
   function WeatherService($http){
     var passphrase = 'i like cheese and bacon and some other things too';
     var baseUrl = 'https://quiet-bayou-88937.herokuapp.com/';
+    var config = {
+      headers: {
+        'passphrase': passphrase
+      }
+    };
     var service = {
       getHourlyData: getHourlyData,
       getMinutelyData: getMinutelyData,
@@ -15,20 +20,30 @@
     };
     return service;
 
+
+
     function getHourlyData(lat, lon){
       var url = baseUrl + 'forecast/hourly/' + lat + ',' + lon;
-      var config = {
-        headers: {
-          'passphrase': passphrase
-        }
-      };
       return $http.get(url, config)
                   .then(function(response){
                     service.weather = response.data;
-                    console.log(service.weather);
                   });
     }
-    function getMinutelyData(lat, lon){}
-    function getDailyData(lat, lon){}
-  }
+
+    function getMinutelyData(lat, lon){
+      var url = baseUrl + 'forecast/Minutely/' + lat + ',' + lon;
+      return $http.get(url, config)
+                  .then(function(response){
+                    service.weather = response.data;
+                  });
+    }
+
+    function getDailyData(lat, lon){
+      var url = baseUrl + 'forecast/Daily/' + lat + ',' + lon;
+      return $http.get(url, config)
+                  .then(function(response){
+                    service.weather = response.data;
+                  });
+    }
+  };
 })();
